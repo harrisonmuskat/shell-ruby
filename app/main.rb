d7933d31b7d545680a1bfacd316a1606dba4c5fa
Harrison Muskat <engineering@muskat.dev>
2025-09-18 12:43:30 -0600

    fix: correctly check path

--- app/main.rb
+++ app/main.rb
@@ -1,36 +1,40 @@
 BUILTINS = [
-    "echo",
-    "exit",
-    "type"
+    'echo',
+    'exit',
+    'type'
 ]
 
-def command?(name)
-  `which #{name}`
-  $?.success?
+def check_path(executable)
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      file = File.join(path, executable)
+      return file if File.executable?(file) && !File.directory?(file)
+    end
+    nil
 end
 
 while true
     # Show the prompt
-    $stdout.write("$ ")
+    $stdout.write('$ ')
     # Wait for user input
-    command, *args = gets.chomp.split(" ")
+    command, *args = gets.chomp.split(' ')
 
     case command
-    when "type"
+    when 'type'
         if(BUILTINS.include? args[0])
-            puts "#{args[0]} is a shell builtin"            
+            puts "#{args[0]} is a shell builtin"       
         else
-            if command?(args[0])
-                "#{args[0]} is " + `which #{args[0]}`.chomp
-            else
+            result = check_path(args[0])
+            if result
+                puts "#{args[0]} is #{result}"
+            else 
                 puts "#{args[0]}: not found"
             end
         end
-    when "exit"
+    when 'exit'
         puts "#{exit} #{args[0]}"
-    when "echo"
-        puts args.join(" ")
+    when 'echo'
+        puts args.join(' ')
     else
-        puts "#{command}: command not found"        
+        puts "#{command}: command not found"     
     end
 end
