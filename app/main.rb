a4075510e84d91f0febf49b4b5289fb006f029d0
Harrison Muskat <engineering@muskat.dev>
2025-09-17 21:12:50 -0600

    feat: move into a case

--- app/main.rb
+++ app/main.rb
@@ -4,7 +4,10 @@ while true
     # Wait for user input
     command, *args = gets.chomp.split(" ")
 
-    if command 
-        puts "#{command}: command not found"
+    case command
+    when exit
+        puts "#{exit} #{args[0]}"
+    else
+        puts "#{command}: command not found"        
     end
 end
