c2b21bd42de73228e6b17037fc7246fe0b24e067
Harrison Muskat <engineering@muskat.dev>
2025-09-17 21:07:14 -0600

    feat: super basic repl

--- app/main.rb
+++ app/main.rb
@@ -1,9 +1,11 @@
 # Uncomment this block to pass the first stage
 $stdout.write("$ ")
 
-# Wait for user input
-command, *args = gets.chomp.split(" ")
+while true
+  # Wait for user input
+    command, *args = gets.chomp.split(" ")
 
-if command 
-    puts "#{command}: command not found"
+    if command 
+        puts "#{command}: command not found"
+    end
 end
